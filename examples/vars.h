b0f9c0cba2c35b123e0ba51105e2620fd8e355b1
Rustam <punk60826@gmail.com>
2026-09-19 23:24:55 +0300

    updated

--- examples/vars.h
+++ examples/vars.h
@@ -8,7 +8,7 @@
 #define	BUFFSIZE	8192
 #define	SA	struct sockaddr
 #define HTTP_PORT 80
-
+#define CPU_VENDOR_OS "sparc64-unknown-freebsd5.1"
 #define	LISTENQ		1024
 
 #endif
--- main.c
+++ main.c
@@ -10,44 +10,27 @@
 #include <sys/errno.h>
 #include <time.h>
 #include "vars.h" 
+#include <netinet/in.h>
+#include	<sys/types.h>
 
 
 int main(int argc, char **argv)
 {
-  int sockfd, n;
-   	char				recvline[MAXLINE + 1];
-  struct sockaddr_in6 servaddr;
+union {
+	  short  s;
+      char   c[sizeof(short)];
+    } un;
 
-  if(argc != 2)
-  {
-   fprintf(stderr, "Usage: %s <IPaddress>\n", argv[0]);
-    return EXIT_FAILURE;
-  }
-  if((sockfd = socket(AF_INET6, SOCK_STREAM, 0)) < 0)
-  {
-      perror("socket error");
-    return EXIT_FAILURE; 
-  }
-  memset(&servaddr, 0, sizeof(servaddr));
-  	servaddr.sin6_family = AF_INET6;	
-  servaddr.sin6_port   = htons(HTTP_PORT);
-if (inet_pton(AF_INET6, argv[1], &servaddr.sin6_addr) <= 0) {
-      fprintf(stderr, "inet_pton error for %s: not a valid IPv4 address\n", argv[1]);
-          close(sockfd);
-    return EXIT_FAILURE;
-}
-if(connect(sockfd, (SA *) &servaddr, sizeof(servaddr)) < 0){
-fprintf(stderr, "connect to %s: %s\n", argv[1], strerror(errno));
-close(sockfd);
-    return EXIT_FAILURE;
-}
-printf("Connected to %s\n", argv[1]);   
-while((n = read(sockfd, recvline, MAXLINE)) > 0){
-  	recvline[n] = 0;
-     
-    if(fputs(recvline, stdout) == EOF){
-          perror("fputs error");
-    }
-}
-  return EXIT_SUCCESS;
+	un.s = 0x0102;
+	printf("%s: ", CPU_VENDOR_OS);
+	if (sizeof(short) == 2) {
+		if (un.c[0] == 1 && un.c[1] == 2)
+			printf("big-endian\n");
+		else if (un.c[0] == 2 && un.c[1] == 1)
+			printf("little-endian\n");
+		else
+			printf("unknown\n");
+	} else
+		printf("sizeof(short) = %zu\n", sizeof(short));
+
 }
